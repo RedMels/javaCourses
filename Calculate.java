d80bf6a43962aa4e0a0ae5c50f3036277bf95bca
RedMels <a.zadorkin1@yandex.ru>
2019-01-24 17:18:22 +0300

    update-test-2 Calculate

--- Calculate.java
+++ Calculate.java
@@ -1,11 +1,45 @@
+import java.util.Scanner;
+
 public class Calculate
 {
 	public static void main(String[] args)
-	{
-		System.out.println("calculate");
-		int first = Integer.valueOf(args[0]);
-		int second = Integer.valueOf(args[1]);
-		int summ = first + second;
-		System.out.println("sum = " + summ);
+	{	
+		Scanner input = new Scanner(System.in);
+		double first;
+		double second;
+		char operation;
+		double result;
+		
+		System.out.println("give me the first number");
+		first = input.nextDouble();
+		
+		System.out.println("give me the second number");
+		second = input.nextDouble();
+		
+		System.out.println("then tell me what to do (+, -, * or /)");
+		operation = input.next().charAt(0);
+
+		
+		switch (operation)
+		{
+			case '+':
+				result = first + second;
+				System.out.println("the result is = " + result);
+				break;
+			case '-':
+				result = first - second;
+				System.out.println("the result is = " + result);
+				break;
+			case '*':
+				result = first * second;
+				System.out.println("the result is = " + result);
+				break;
+			case '/':
+				result = first / second;
+				System.out.println("the result is = " + result);
+				break;
+			default:
+				System.out.println("irregular input");
+		}
 	}
 }
